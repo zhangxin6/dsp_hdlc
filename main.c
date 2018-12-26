99a873d5cd5092f64bcc1c2dab8332bba76d17ce
zhangxin <zhangxin_mail163@163.com>
2018-12-26 18:24:55 +0800

    hdlc with all ok!

--- main.c
+++ main.c
@@ -10,10 +10,12 @@
 /********************************************************************
  * 可修改的全局变量
  *******************************************************************/
-#define DEBUG
+//#define DEBUG
 //记录速度
 CSL_Uint64 average_write_speed=0;
 CSL_Uint64 average_read_speed=0;
+unsigned int frame=0;
+unsigned int hdlc_rec_flag =0;
 //DMA测试宏变量
 #define DEVICE_HZ         1000000000//800MHZ:800000000//1GHZ:1000000000;1.25GHZ:1250000000
 #define MB   		      0x100000
@@ -29,65 +31,48 @@ void main(void)
 {
     init();
 	int coreNum=0;
-	int * src_buf= (int *)EMIF16FPGA_SEND_BUF_ADDR;
-    #if GPIO_LOOP_BACK_TEST
-        GPIO_loopback_test();
-    #else
-        int j;
-        if(DEVICE==DEVICE1)
+    int * src_buf= (int *)EMIF16FPGA_SEND_BUF_ADDR;
+    int * dst_buf= (int *)EMIF16FPGA_RECV_BUF_ADDR;
+    unsigned int length,i;
+	coreNum = CSL_chipReadReg (CSL_CHIP_DNUM);
+    if(DEVICE==DEVICE1)
+    {
+        if (coreNum ==0)
         {
-            if (coreNum ==0)
+            while(1)
             {
-                while(1)
+                if(hdlc_rec_flag == 1)
                 {
-                    /*for(j=0;j<512;j++)
+                    hdlc_rec_flag = 0;
+                    frame=frame+1;
+                    length = receive_hdlc(dst_buf);
+                    if( (frame%3)==1 )
                     {
-                        //src_buf[j] = 255;
-                        src_buf[j] = (j + 3)%256;
+                        src_buf[0]=0x77;
+                        src_buf[1]=0x22;
+                        src_buf[2]=0x44;
+                        //src_buf[2]=frame/3;
+                        for(i=1;i<200;i++)
+                        {
+                            src_buf[i+2] = i+1 ;
+                        }
+                        send_hdlc(202,src_buf);
                     }
-                    send_hdlc(500,src_buf);*/
-                    //int * dst_buf= (int *)EMIF16FPGA_RECV_BUF_ADDR;
-                    //unsigned short length;
-                    //length = receive_hdlc(dst_buf);
-                    C6678_TimeCounter_Delaycycles(10000000);
                 }
+                C6678_TimeCounter_Delaycycles(10000000);
             }
         }
-    #endif
+    }
 }
 
 //Interrupt service routine for timer
 void interrupt GPIO_ISR(void)
 {
 	Uint32 eventFlag;
-	unsigned int i,error;
-	unsigned short length;
-	int * dst_buf= (int *)EMIF16FPGA_RECV_BUF_ADDR;
-	int * src_buf= (int *)EMIF16FPGA_SEND_BUF_ADDR;
-
 	/*read interrupt flag*/
 	eventFlag= gpCGEM_regs->MEVTFLAG[2];
-
 	/*clear interrupt flag*/
 	gpCGEM_regs->EVTCLR[2]= eventFlag;
 
-	//shift the interrupt register to match the GPIO data register
-	#ifdef DEBUG
-		printf("GPIO interrupt event flag on CorePac INTC = 0x%x\n",((eventFlag&(~(1<<(CSL_GEM_GPINTN-64))))>>(10))|((eventFlag>>(CSL_GEM_GPINTN-64))<<DNUM));
-	#else
-		;
-	#endif
-	
-	length = receive_hdlc(dst_buf);
-	error =0;
-	/*for(i=0;i<length;i++)
-	{
-		if(*src_buf != *dst_buf)
-			error = error + 1;
-	}
-	#ifdef DEBUG
-		printf("error = %d\n",error);
-	#else
-		;
-	#endif*/
+	hdlc_rec_flag = 1;
 }
